7b5c0017cf27cd8d1f608fc1b1fba5c792f876cf
Xavier Miles <xaviermiles377@gmail.com>
2026-07-03 09:26:29 +1200

    Add a backend package

---
+++ backend/src/main.rs
@@ -0,0 +1,15 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // run it with hyper on localhost:3000
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
