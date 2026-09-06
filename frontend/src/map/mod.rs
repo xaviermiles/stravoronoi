13aead793c67b03e416a5b28736a6fad50d824eb
xaviermiles <xaviermiles377@gmail.com>
2026-09-06 15:02:53 +1200

    Add carto API key and switch from raster->vector

--- frontend/src/map/mod.rs
+++ frontend/src/map/mod.rs
@@ -3,11 +3,9 @@ use crate::strava::{self, LoadState};
 use boostvoronoi::prelude::*;
 use chrono::{DateTime, Utc};
 use geojson::{Feature, GeoJson};
-use mapboxgl::Source;
-use mapboxgl::layer::{CircleLayer, IntoLayer, Layer, RasterLayer};
+use mapboxgl::layer::CircleLayer;
 use mapboxgl::layer::{LineCap, LineJoin, LineLayer};
-use mapboxgl::style::Sources;
-use mapboxgl::{LngLat, Map, MapEventListener, MapOptions, Style, event};
+use mapboxgl::{LngLat, Map, MapEventListener, MapOptions, event};
 use std::time::Duration;
 use std::{cell::RefCell, rc::Rc};
 use web_sys::wasm_bindgen::JsCast;
@@ -18,6 +16,7 @@ use yew::{use_effect_with_deps, use_mut_ref};
 mod hit_testing;
 use hit_testing::distance_to_run_squared;
 
+const CARTO_API_KEY: &str = env!("CARTO_API_KEY");
 const MAPBOX_TOKEN: &str = env!("MAPBOX_TOKEN");
 
 /// Shared handle to the map, populated once the map has been created.
@@ -407,40 +406,11 @@ pub fn set_grid_visible(map: &Map, visible: bool) {
 }
 
 fn create_map() -> Rc<Map> {
-    let mut sources = Sources::new();
-    sources.insert(
-        "carto-light".into(),
-        Source {
-            r#type: "raster".into(),
-            // The @2x is to avoid upscaling blue to make the rendering sharper on HiDPI screens.
-            tiles: Some(vec![
-                "https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png".into(),
-                "https://b.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png".into(),
-                "https://c.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png".into(),
-                "https://d.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png".into(),
-            ]),
-            ..Default::default()
-        },
-    );
-    let layers: Vec<Layer> = vec![
-        RasterLayer {
-            id: "carto-light-layer".into(),
-            source: "carto-light".into(),
-            minzoom: Some(0.0),
-            maxzoom: Some(21.0),
-            ..Default::default()
-        }
-        .into_layer(),
-    ];
-
     // The default coordinates are Christchurch.
     let opts = MapOptions::new(MAPBOX_TOKEN.into(), "map".into())
-        .style(Style {
-            version: 8,
-            sources,
-            layers,
-            ..Default::default()
-        })
+        .style_ref(format!(
+            "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json?key={CARTO_API_KEY}"
+        ))
         .center(LngLat::new(172.637491, -43.530950))
         .zoom(13.0);
 
